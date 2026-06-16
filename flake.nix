280234cd58cfec9260e8c9cb55109d39a52e10dd
Luke <luke@lpdev.io>
2026-06-16 17:05:15 +1000

    chore: update nix flake to use 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "vite react app dev shell";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   };
 
   outputs = { self, nixpkgs }:
